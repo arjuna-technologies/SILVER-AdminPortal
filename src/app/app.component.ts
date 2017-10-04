58033906a28fc832340658a5ad87b5e69dfbb41a
Stuart Wheater <stuart.wheater@arjuna.com>
2017-10-04 17:03:40 +0100

    Initial skeliton implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+//
+// Copyright (c) 2017, Arjuna Technologies Limited, Newcastle upon Tyne, England,
+//                     Open Lab, Newcastle University, Newcastle upon Tyne, England,
+//                     Institute of Health and Society, Newcastle University, Newcastle upon Tyne, England.
+//                     All rights reserved.
+//
+
+import { Component } from '@angular/core';
+import { MdDialog } from '@angular/material';
+
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+
+@Component
+({
+    selector:    'silver-root',
+    templateUrl: './app.component.html',
+    styleUrls:   ['./app.component.scss']
+})
+export class AppComponent
+{
+    public username: string;
+
+    public constructor(private dialog: MdDialog)
+    {
+        this.username = '';
+    }
+
+    public openLoginDialog(): void
+    {
+        if (this.username === '')
+        {
+            const loginDialogRef = this.dialog.open(LoginDialogComponent);
+        }
+        else
+        {
+            this.username = '';
+        }
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import 'hammerjs';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdDialogModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+
+@NgModule
+({
+    declarations:
+    [
+        AppComponent,
+        LoginDialogComponent
+    ],
+    imports:
+    [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MdToolbarModule,
+        MdIconModule,
+        MdButtonModule,
+        MdDialogModule
+    ],
+    entryComponents:
+    [
+        LoginDialogComponent
+    ],
+    providers:
+    [
+    ],
+    bootstrap:
+    [
+        AppComponent
+    ]
+})
+export class AppModule
+{
+}
